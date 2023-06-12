ece3aee2cc37fc403b8585ec4827cef5b098de1b
Andrew Kent <AJEKAKENT@gmail.com>
2023-06-12 00:55:47 -0400

    directory iterator and scan function

--- src/directory.cpp
+++ src/directory.cpp
@@ -1,13 +1,29 @@
 #include <boost/filesystem.hpp>
+#include <string>
 
+#include "pexceptions.h"
 #include "directory.h"
 
 // TODO: it is possible that boost::filesystem has a method of storing the directories and I am overthinking it
 
 
-void Directory_tracker::save(std::string path) {
+void Directory_tracker::save_text(std::string path) {
     
 }
+
+void Directory_tracker::write_text(std::ostream os){
+
+}
+
+void Directory_tracker::save_xml(std::string path) {
+    
+}
+
+void Directory_tracker::write_xml(std::ostream os){
+
+}
+
+
 
 void Directory_tracker::scan(){
     int depth;
@@ -32,17 +48,18 @@ void Directory_tracker::scan(){
             node->right = prev_node;
             node->parent = prev_node->parent;
         }
-        else if (prev_depth + 1 == fs.depth()) { // child in new dir
+        else if (prev_depth < fs.depth()) { // child in new dir
+            prev_node = level_jump(fs.depth() - prev_depth, prev_node);
             prev_node->child = node;
             node->parent = prev_node;
         }
-        else if (prev_depth - 1 == fs.depth()) { // parent sibling
-            prev_node->parent->left = node;
-            node->right = prev_node->parent;
+        else if (prev_depth > fs.depth()) { // parent sibling
+            prev_node = level_jump(fs.depth() - prev_depth, prev_node);
+            prev_node = node;
+            node->right = prev_node;
         }
         else {
-            // TODO: figure out base case for popout of n directories
-            throw std::runtime_error("ERROR: Jumped out of or into multiple directories");
+            throw permstat::dir_error("ERROR: Directory_tracker.Scan(). You aren't supposed to be here\nPlease submit a bug report");
             return;
         }
         
@@ -50,5 +67,74 @@ void Directory_tracker::scan(){
         prev_node = node;
         prev_depth = depth;
         fs.next();
+    }
+}
+
+
+Node* Directory_tracker::level_jump(int diff, Node* base){
+    Node* node = base;
+    for(int i = 0; i < diff; ++i){
+        if (node == nullptr){
+            throw permstat::dir_error("Error: level_jump: Depth out of range.");
+        }
+        if (diff < 0){
+            node = node->parent;
+        }
+        else {
+            node = node->child;
+        }
+    }
+    return node;
+}
+
+Node* Directory_tracker::iterator::operator*() {
+    return node;
+};
+
+void Directory_tracker::iterator::next(){
+    if (!good){
+        throw permstat::dir_error("ERROR: Directory_tracker::iterator.next() use of bad iterator: nullptr");
+    }
+
+    if (node->child) {          // jump into this file
+        node = node->child;
+    }
+    else if (node->left) {      // go to next file
+        node = node->left;
+    }
+    else {                      // Go up to next parent file
+        while (node->parent->left == nullptr){
+            if (node == nullptr){
+                good = 0;
+                return;
+            }
+            else {
+                node = node->parent;
+            }
+        }
+        node = node->parent->left;
+    }
+}
+
+void Directory_tracker::iterator::prev(){// TODO: finish
+    if (!good){
+        throw permstat::dir_error("ERROR: Directory_tracker::iterator.prev() use of bad iterator: nullptr");
+    }
+
+    if (node->right == nullptr){            // up to parent
+        if (node->right->parent == nullptr){
+            good = 0;
+            return;
+        }
+        node = node->parent;
+    }
+    else if (node->right->child == nullptr){ // right to next
+        node = node->right;
+    }
+    else {                                  // down to child
+        node = node->right;
+        while (node->child != nullptr) {
+            node = node->child;
+        }
     }
 }
--- src/directory.h
+++ src/directory.h
@@ -42,12 +42,38 @@ class Directory_tracker {
     Directory_tracker(std::string directory_to_scan) : fs(Filesystm(directory_to_scan)) {}
     ~Directory_tracker(){};
 
-    void save(std::string path);
+    void save_text(std::string path);
+    void save_xml(std::string path);
     void scan();
+
+    Node* end(){ return nullptr; }
+    Node* begin(){ return &root; }
+
 
 private:
     Node root;
     Filesystm fs;
+
+    void write_text(std::ostream os);
+    void write_xml(std::ostream os);
+    Node* level_jump(int diff, Node* base);
+
+
+
+public:
+    class iterator {
+        iterator() : node(nullptr), good(0) {};
+        iterator(Node* n) : node(n), good(1) {};
+        void next();
+        void prev();
+
+        Node* operator*();
+        bool isgood(){ return good; }
+
+    private:
+        Node* node;
+        bool good = 1;
+    };
 };
 
 
